c6face2ba15722139158ea83f69fc182b783e7fb
BlazeEdge <vladiviral2020@gmail.com>
2025-11-11 20:44:29 +0300

    Player HP-bar
    Добавил Полоску ХП игрока

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,25 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public GameObject player;
     public int xp = 3;
     public GameObject goose;
-    
+    public Image hp_img;
 
     void Start()
     {
         xp = 4;
         goose.SetActive(false);
     }
+   
 
     private void OnTriggerEnter(Collider other)
     {
-
+        Debug.Log(xp);
         if (other.CompareTag("Enemy"))
         {
+
             xp -= 1;
             Destroy(other.gameObject);
             if (xp <= 0)
@@ -27,6 +30,9 @@ public class PlayerHealth : MonoBehaviour
                 goose.SetActive(true);
                 Destroy(player);
             }
+            // Debug.Log(xp);
+            // Debug.Log(xp / 4);
+            hp_img.fillAmount = xp / 4f;
         }
     }
 }
